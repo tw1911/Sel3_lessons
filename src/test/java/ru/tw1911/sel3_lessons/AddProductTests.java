04227259b9c6669f5b66d54bef721ba8d25309b5
tw1911 <tw1911@yandex.ru>
2019-03-01 08:46:24 +0300

    addProduct some fields

--- src/test/java/ru/tw1911/sel3_lessons/AddProductTests.java
+++ src/test/java/ru/tw1911/sel3_lessons/AddProductTests.java
@@ -3,6 +3,7 @@ package ru.tw1911.sel3_lessons;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddProductTests extends BasicTest{
 
@@ -16,6 +17,13 @@ public class AddProductTests extends BasicTest{
         driver.findElement(By.cssSelector("input[name='code']")).sendKeys(RandomStringUtils.randomNumeric(8));
         driver.findElement(By.xpath("//td[strong='Product Groups']//td[text()='Female']/preceding-sibling::td")).click();
         driver.findElement(By.name("quantity")).sendKeys("5");
+        String filepath = System.getProperty("user.dir")+"/src/test/resources/PutIn.jpg";
+        driver.findElement(By.cssSelector("input[type='file']")).sendKeys(filepath);
+        driver.findElement(By.xpath("//div[@class='tabs']//a[text()='Information']")).click();
 
+        Select manufactorer = new Select(driver.findElement(By.name("manufacturer_id")));
+        manufactorer.selectByVisibleText("ACME Corp.");
+        driver.findElement(By.name("keywords")).sendKeys("put in toy");
+        driver.findElement(By.name("short_description[en]")).sendKeys("put in toy to play with bear and yellow duck");
     }
 }
