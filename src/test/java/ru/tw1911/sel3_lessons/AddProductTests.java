f1a4a899c68bf9a705d37d8387464411fd2c75bd
tw1911 <tw1911@yandex.ru>
2019-03-06 23:37:06 +0300

    ex12 raw

--- src/test/java/ru/tw1911/sel3_lessons/AddProductTests.java
+++ src/test/java/ru/tw1911/sel3_lessons/AddProductTests.java
@@ -4,7 +4,11 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.UUID;
 
 public class AddProductTests extends BasicTest{
 
@@ -14,7 +18,8 @@ public class AddProductTests extends BasicTest{
         adminHelper.clickMenuItem("Catalog");
         driver.findElement(By.xpath("//a[contains(text(),'Add New Product')]")).click();
         driver.findElement(By.xpath("//label[contains(text(),'Enabled')]/input")).click();
-        driver.findElement(By.cssSelector("input[name='name[en]']")).sendKeys("someProduct");
+        String productName = "someProduct"+ UUID.randomUUID().toString().substring(15);
+        driver.findElement(By.cssSelector("input[name='name[en]']")).sendKeys(productName);
         driver.findElement(By.cssSelector("input[name='code']")).sendKeys(RandomStringUtils.randomNumeric(8));
         driver.findElement(By.xpath("//td[strong='Product Groups']//td[text()='Female']/preceding-sibling::td")).click();
         driver.findElement(By.name("quantity")).sendKeys("5");
@@ -27,5 +32,22 @@ public class AddProductTests extends BasicTest{
         driver.findElement(By.name("keywords")).sendKeys("put in toy");
         driver.findElement(By.name("short_description[en]")).sendKeys("put in toy to play with bear and yellow duck");
         driver.findElement(By.className("trumbowyg-editor")).sendKeys("put in toy to play with bear and yellow duck"+Keys.ENTER+"Logn description");
+        driver.findElement(By.name("head_title[en]")).sendKeys("PutIn yellow duck");
+        driver.findElement(By.name("meta_description[en]")).sendKeys("putIn yellow duck");
+
+        driver.findElement(By.xpath("//div[@class='tabs']//a[text()='Prices']")).click();
+
+        WebElement priceField = driver.findElement(By.name("purchase_price"));
+        priceField.clear();
+        priceField.sendKeys("100");
+
+        Select priceCurrency = new Select(driver.findElement(By.name("purchase_price_currency_code")));
+        priceCurrency.selectByValue("USD");
+        driver.findElement(By.name("prices[USD]")).sendKeys("100");
+        driver.findElement(By.name("prices[EUR]")).sendKeys("80");
+
+        driver.findElement(By.name("save")).click();
+        WebElement dataTable = driver.findElement(By.className("dataTable"));
+        dataTable.findElement(By.xpath(String.format(".//tr/td/a[text()='%s']",productName))).isDisplayed();
     }
 }
