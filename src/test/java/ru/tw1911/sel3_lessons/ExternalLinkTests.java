bdde770dc0453c13beaf7ea53779d0c3763bbb45
tw1911 <tw1911@yandex.ru>
2019-03-15 16:08:57 +0300

    exercise 14 final

--- src/test/java/ru/tw1911/sel3_lessons/ExternalLinkTests.java
+++ src/test/java/ru/tw1911/sel3_lessons/ExternalLinkTests.java
@@ -3,9 +3,9 @@ package ru.tw1911.sel3_lessons;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import ru.tw1911.sel3_lessons.BasicTest;
 
 import java.util.List;
+import java.util.Set;
 
 public class ExternalLinkTests extends BasicTest {
 
@@ -16,6 +16,18 @@ public class ExternalLinkTests extends BasicTest {
         driver.findElement(By.xpath("//a[contains(text(),'Add New Country')]")).click();
         List<WebElement> externalLinks = driver.findElements(By.cssSelector("form [target='_blank']"));
         externalLinks.forEach(webElement -> System.out.println(webElement.getAttribute("href")));
+        for(WebElement link: externalLinks){
+            String mainWindow = driver.getWindowHandle();
+            Set<String> oldWindows = driver.getWindowHandles();
+            link.click(); // открывает новое окно
+            String newWindow = wait.until(webDriver -> {
+                Set<String> newWindows = webDriver.getWindowHandles();
+                newWindows.removeAll(oldWindows);
+                return newWindows.iterator().next();
+            });
+            driver.switchTo().window(newWindow);
+            driver.close();
+            driver.switchTo().window(mainWindow);
+        }
     }
-
 }
